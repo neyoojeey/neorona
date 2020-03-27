32b8c4d352d84eaa95718eaa7d2094e5030ab871
Neo Jay Andong <neojay.andong@pueblodepanay.com>
2020-03-27 11:04:30 +0800

    Fix colors

--- src/components/Home/Home.Component.jsx
+++ src/components/Home/Home.Component.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { IonText, IonRouterLink } from '@ionic/react';
+import React from "react";
+import { IonText, IonRouterLink } from "@ionic/react";
 
-import CountrySelector from '../CountrySelector/CountrySelector.Component';
+import CountrySelector from "../CountrySelector/CountrySelector.Component";
 
 const HomeContent = props => {
     return (
         <div className='container ion-margin-top'>
             <h1 className='ion-text-center'>Welcome to Corona Stats</h1>
             <IonText color='dark'>
-                The stats showed below are based on{' '}
+                The stats showed below are based on{" "}
                 <IonRouterLink
                     href='https://github.com/mathdroid/covid-19-api'
                     target='_blank'
                     color='primary'>
-                    https://github.com/mathdroid/covid-19-api
+                    https://github.com/mathdroid/covid-19-api.
                 </IonRouterLink>
             </IonText>
             <CountrySelector url='https://covid19.mathdro.id/api/countries/'></CountrySelector>
